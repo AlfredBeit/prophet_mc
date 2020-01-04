6d8872787e342e4db654725d8d80702490a201f5
alfredbeit <abittaraev@gmail.com>
2020-01-04 19:58:11 +0300

    comments added, unnecessary objects removed

--- prophet_ex.R
+++ prophet_ex.R
@@ -1,49 +1,51 @@
 library("dplyr")
 library("parallel")
-install.packages("prophet", type="binary")
+#install.packages("prophet", type="binary")
 library("prophet")
 
 
 rm(list=ls())
 
-
 dir<-paste0(getwd(),'/parallel/data - data.csv')
 
 df <- read.csv(dir, header=FALSE, stringsAsFactors = FALSE)
+names_df<-c("client_id", "yearmon","receiver_id", "sender_id" , "volume")
+names(df)<-names_df
 
-df$V2<-(paste0(df$V2,"01"))
+df$yearmon<-(paste0(df$yearmon,"01")) # add day to yearmon dates
 
 
-
-
+# tot_vol is the volume of orders summed monthly over each client_id
 agg_df<-df %>%
-          mutate(dates= lubridate::ymd(V2)) %>%
-          group_by(dates,V1)%>%
-          summarise(tot_vol = sum(V5))%>%
-          ungroup()%>%
-          arrange(V1,dates)
+  mutate(dates= lubridate::ymd(yearmon)) %>%
+  group_by(dates,client_id)%>%
+  summarise(tot_vol = sum(volume))%>%
+  ungroup()%>%
+  arrange(client_id,dates)
 
-names<-base::unique(df$V1)
-series <-  vector("list", length = length(names))
-plots <-  vector("list", length = length(names))
+# create a fake dict with each position of the list being ("key", "value") pair
+# and ("key", "value") being (unique client_id, list containing the corresponding time-series)
+
+names_dict<-base::unique(df$client_id)
+series <-  vector("list", length = length(names_dict))
+names(series)<-names_dict
 
 
-names(series)<-names
-names(plots)<-names
 
 getModel<-function(s) {
 
-  series[[s]]<- agg_df%>%
-                filter(V1==names[s])%>%
-                select(ds=dates,y = tot_vol)
-  m <- prophet(series[[s]], 
-                            daily.seasonality = FALSE, 
+  # fill in the "dict"
+  series[[s]]<- agg_df %>%
+    filter(client_id==names_dict[s]) %>%
+    select(ds=dates,y = tot_vol)
+  
+  # create the fbprohet model object
+  m <- prophet(series[[s]], daily.seasonality = FALSE, 
                             weekly.seasonality = FALSE,
                             yearly.seasonality = FALSE)
   return(m)
   
 }
-
 
 
 getForecast<-function(s, forward=5) {
@@ -52,12 +54,13 @@ getForecast<-function(s, forward=5) {
   fc<-predict(model, future)
   return( list(data=tail(fc[c("ds","yhat_lower", "yhat", "yhat_upper")]), 
                plot=dyplot.prophet(model,fc)) )
-  }
+}
 
 Forecasts <- parallel::mclapply(1:5,FUN = getForecast)
 
+#examples of interactive dyplots
+
 Forecasts[[1]]$plot
 Forecasts[[5]]$plot
-
 
 
